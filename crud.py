7d3579cfd1972865e7aaf7a2a9a764a8d77aff13
Lucas Rasia <lucasrasia@outlook.com>
2026-07-14 14:50:11 -0300

    início de lógica de repetição

--- crud.py
+++ crud.py
@@ -23,5 +23,17 @@ def palavras_revisar(session):
     resultado=session.query(Palavra).join(Status).filter_by(Status.due_date<=date.today()).all()
     return resultado
 
-def pos_estudo(session, resposta):
-    
+def pos_estudo(session, resposta, word_id):  #resposta = fácil/difícil/errei
+    intervalo=[1, 3, 7, 14, 30, 90] # cada índice é um nível de conhecimento
+    status_word=session.query(Status).filter(Status.word_id==word_id).first()
+    if not status_word:
+        raise ValueError("Status não encontrado")
+    if resposta=="fácil":
+        status_word.step=0
+    if resposta=="dificil":
+        ...
+    else:       #errei
+        status_word.step=0  #aparecer na próxima sessão
+    status_word.due_date=date.today()+status_word.step
+    session.commit()
+    return status_word
