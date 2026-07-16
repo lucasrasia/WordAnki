8536959f80a00cb30744a704398bd8e7345e4e2c
Lucas Rasia <lucasrasia@outlook.com>
2026-07-16 20:48:11 -0300

    rota API de criar palavra

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from sqlalchemy.orm import Session
-from datetime import date, timedelta
+from crud import criar_palavra, criar_status, deletar_palavra, avaliar, palavras_revisar
 from models import Palavra, Status
-from crud import criar_palavra, criar_status
+from database import session
 
+from fastapi import FastAPI, HTTPException, status, Path
+from typing import Optional
+from pydantic import BaseModel  #"Espero receber json"
+
+app=FastAPI()
+
+#criar palavra
+class NovaPalavra(BaseModel):   #o React retorna em json: {"nome": "apple", "traducao": "maçã", "frase": "I eat an apple"}
+    nome: str
+    traducao: str
+    frase: str
+
+@app.post('Palavras')
+def criar(dados: NovaPalavra):
+    print(dados.nome)
+    print(dados.traducao)
+    return{'ok': True}
