8f803a4d51c3e943b67a185d586572566eec7708
Lucas Rasia <lucasrasia@outlook.com>
2026-07-16 21:48:24 -0300

    feat: cascade, se eu deletar uma palavra de Palavra, ela é deletada de Status automaticamente

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, Column, String, Integer,  Date, ForeignKey
+from sqlalchemy.orm import relationship
 from datetime import date, timedelta
 
 from database import Base, engine
@@ -10,12 +11,16 @@ class Palavra(Base): #nome no singular, na tabela fica "palavras" -> importante 
     traducao= Column("tradução", String)
     frase= Column("frase", String) 
 
+    status = relationship("Status", back_populates="palavra", cascade="all, delete-orphan")
+
 class Status(Base):
     __tablename__ = "status"
     id=Column("id", Integer, primary_key=True, autoincrement=True)
     word_id=Column(Integer, ForeignKey("palavras.id"), unique=True)
     due_date=Column(Date, default=date.today) #data que a palavra deve aparecer
     step=Column(Integer, default=0) #intervalo
+
+    palavra = relationship("Palavra", back_populates="status")
 
 Base.metadata.create_all(bind=engine) #cria as tabelas dentro do db
 
